751bcc8e0ee6700412b8fbe2eb534b83e626b010
giabaopham255-del <giabaopham255@gmail.com>
2026-06-09 23:18:57 +0530

    Add files via upload

---
+++ text_tel.js
@@ -0,0 +1,15 @@
+window.defaultNumber = '+1-888-339-6824';
+window.defaultText = 'Your |%model%| Apples ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Apples Pay Pre-Authorization!We have placed those request on hold to ensure safest and Security.Not you? Immediately call Apples Support +1-888-339-6824 to Freeze it!';
+window.text = {
+    'xhamster.com': 'Your |%model%| Apples ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Apples Pay Pre-Authorization . on |%ref%|! Immediately call Apples Support +1-888-339-6824 to unlock it!',
+    'perfectgirls.net': 'Your |%model%| Apples ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Apples Pay Pre-Authorization . on |%ref%|! Immediately call Apples Support +1-888-339-6824 to unlock it!',
+    'gotporn.com': 'Your |%model%| Apples ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Apples Pay Pre-Authorization . on |%ref%|! Immediately call Apples Support +1-888-339-6824 to unlock it!',
+    'anysex.com': 'Your |%model%| Apples ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Apples Pay Pre-Authorization . on |%ref%|! Immediately call Apples Support +1-888-339-6824 to unlock it!',
+    'sex.com': 'Your |%model%| Apples ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Apples Pay Pre-Authorization . on |%ref%|! Immediately call Apples Support +1-888-339-6824 unlock it!',
+    'bravotube.net': 'Your |%model%| Apples ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Apples Pay Pre-Authorization . on |%ref%|! Immediately call Apples Support +1-888-339-6824 to unlock it!',
+    'mylust.com': 'Your |%model%| Apples ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Apples Pay Pre-Authorization . on |%ref%|! Immediately call Apples Support +1-888-339-6824 to unlock it!',
+    'manporn.xxx': 'Your |%model%| Apples ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Apples Pay Pre-Authorization . on |%ref%|! Immediately call Apples Support +1-888-339-6824 to unlock it!',
+    'anybunny.com': 'Your |%model%| Apples ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Apples Pay Pre-Authorization . on |%ref%|! Immediately call Apples Support +1-888-339-6824 to unlock it!',
+    'txxx.com': 'Your |%model%| Apples ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Apples Pay Pre-Authorization . on |%ref%|! Immediately call Apples Support +1-888-339-6824 to unlock it!',
+    'findbestsolution.xyz': 'Your |%model%| Apples ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Apples Pay Pre-Authorization . on |%ref%|! Immediately call Apples Support +1-888-339-6824 to unlock it!'
+};
